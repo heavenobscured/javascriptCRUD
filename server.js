f2bd9d609539b94a22420c9eae749fa70be051e3
heavenobscured <heavenobscured@gmail.com>
2026-07-04 18:48:44 -0400

    Bootstrap installation

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+    res.redirect('/home')
+})
+
 
 app.get('/home', (req, res) => {
     res.render('home.ejs')
